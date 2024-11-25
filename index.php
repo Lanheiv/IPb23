83a5a6f7f38f6f30a3e6af5984fc66caa87a7bf4
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-25 15:16:55 +0200

    index box

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$savien = new mysqli("localhost", "root", "", "ipb23");
+$savien = new mysqli("localhost", "root", "", "ipb23"); // Pēdējais "" ir priekš datubāzes
 /*
 if ($savien->connect_error) {
   echo("Kļūda: " . $savien->connect_error);
@@ -9,7 +9,13 @@ echo "Strādā";
 
 // var_dump($savien);
 $sql = $savien->query("SELECT * FROM bok");
-$row = $sql->fetch_assoc(); // Dabū datus ārā no objekta
+$data = $sql->fetch_assoc(); // Dabū datus ārā no datubāzes
 // query un fetch_assoc ir iebūvētā metode
 
-var_dump($row);
+var_dump($data);
+
+echo "<form>";
+echo "<input name='username'/>";
+echo "<input name='password' type='password'/>";
+echo "<input type='submit' value='register'";
+echo "</form>";
